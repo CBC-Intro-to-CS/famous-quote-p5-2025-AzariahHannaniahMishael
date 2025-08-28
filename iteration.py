2e92a7abf8a3cfc95bb94e040e390b73fe229f33
Danovan <destind@cbchs.org>
2025-08-28 18:52:24 -0500

    Redo- Code problem 8/28

--- iteration.py
+++ iteration.py
@@ -5,26 +5,24 @@
 # print("Blastoff!")
 
 
-# purchase_tickets = True
-# while purchase_tickets:
-#     print("Type '0' to exit")
-#     age = int(input("How old are you: "))
+purchase_tickets = True
+while purchase_tickets:
+    print("Type '0' to exit")
+    age=int(input("how old are you: "))
 
-#     if age >= 1 and age <= 3:
-#         print("Your ticket is free!")
-#     elif age >= 4 and age <= 11:
-#         print:("Your ticket is $10.00")
-#     elif age >= 12:
-#         print:("Your ticket is $15.00")
-#     elif age ==0:
-#         purchase_tickets = False
+    if age >= 1 and <= 3:
+        print("your ticket is free!")
+    elif age >= 4 and age <= 11:
+        print("your ticket is $10.00")
+    elif age >= 12:
+        print("Your ticket is $15.00")
+    elif age == 0 
+    purchase_tickets= False
 
-# print("Thank you for purchasing tickets!")
-
-count = 0 
-for nums in [3, 41, 12, 9, 74, 15]:
-    count = count + 1
-print("Count: " + str(count))
+# count = 0 
+# for nums in [3, 41, 12, 9, 74, 15]:
+#     count = count + 1
+# print("Count: " + str(count))
 
 
 
