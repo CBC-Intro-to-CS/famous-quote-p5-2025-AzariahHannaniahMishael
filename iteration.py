35c627f8665a9e7a80697865f84699d1a023e25f
Danovan <destind@cbchs.org>
2025-08-29 09:23:45 -0500

    Learned about iteration

--- iteration.py
+++ iteration.py
@@ -5,24 +5,45 @@
 # print("Blastoff!")
 
 
-purchase_tickets = True
-while purchase_tickets:
-    print("Type '0' to exit")
-    age=int(input("how old are you: "))
+# purchase_tickets = True
+# while purchase_tickets:
+#     print("Type '0' to exit")
+#     age=int(input("how old are you: "))
 
-    if age >= 1 and <= 3:
-        print("your ticket is free!")
-    elif age >= 4 and age <= 11:
-        print("your ticket is $10.00")
-    elif age >= 12:
-        print("Your ticket is $15.00")
-    elif age == 0 
-    purchase_tickets= False
+#     if age >= 1 and <= 3:
+#         print("your ticket is free!")
+#     elif age >= 4 and age <= 11:
+#         print("your ticket is $10.00")
+#     elif age >= 12:
+#         print("Your ticket is $15.00")
+#     elif age == 0 
+#     purchase_tickets= False
 
 # count = 0 
 # for nums in [3, 41, 12, 9, 74, 15]:
 #     count = count + 1
 # print("Count: " + str(count))
 
+# magicians= ['alice','david','carolina']
+# for magician in magicians:
+#     print(f"{magician.title()} is a magician.")
+
+# print("Thank you, everyone. That was a good show!")
+
+# Using the Range Function
+for value in range(1, 6):
+    print(value)
+
+    numbers = list(range(1,6))
+print(numbers)
 
 
+even_numbers= list(range(2, 11, 2))
+print(even_numbers)
+
+squares= []
+for value in range(1,11):
+    square = value ** 2
+    squares.append(square)
+
+    print(squares)
